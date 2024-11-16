2f7c94cfd2df9e6cdb84dfb314a890ad7c8942b6
am_021996A <am_odeh@gmail.com>
2024-11-16 15:01:22 +0300

    update

--- src/MyTestCases.java
+++ src/MyTestCases.java
@@ -104,30 +104,12 @@ public class MyTestCases extends Parameters {
 	@Test(priority = 8, enabled = true)
 	public void RandomlyChangeLanguage() throws InterruptedException {
 
-		driver.get(myWebsites[randomIndex]);
-
-		WebElement HotelTab = driver.findElement(By.id("uncontrolled-tab-example-tab-hotels"));
-
-		HotelTab.click();
+		RandomlyCheckTheWebsite();
 
 		WebElement HotelSearchBar = driver.findElement(By.cssSelector(".sc-phbroq-2.uQFRS.AutoComplete__Input"));
 
 		CheckTheWebSiteToEnterCityName(HotelSearchBar);
-		
-
-		WebElement CitiesList = driver.findElement(By.cssSelector(".sc-phbroq-4.gGwzVo.AutoComplete__List"));
-		WebElement SelectNamberOfVistor = driver.findElement(By.cssSelector(".sc-tln3e3-1.gvrkTi"));
-		CitiesList.findElements(By.tagName("li")).get(1).click();
-
-		Select select = new Select(SelectNamberOfVistor);
-		int RandomOfVistor = rand.nextInt(2);
-
-		select.selectByIndex(RandomOfVistor);
-
-		WebElement SearchButton = driver.findElement(By.xpath("//button[@data-testid='HotelSearchBox__SearchButton']"));
-		SearchButton.click();
-
-		Thread.sleep(45000);
+		EnterNumberOfVisetorAndSearchResult();
 
 	}
 
@@ -138,9 +120,7 @@ public class MyTestCases extends Parameters {
 
 		boolean ActualResult = SearchResult.getText().contains("found") || SearchResult.getText().contains("مكان");
 
-		boolean ExpectedResult = true;
-
-		Assert.assertEquals(ActualResult, ExpectedResult);
+		Assert.assertEquals(ActualResult, ExpectedThePageIsFullyLoaded);
 	}
 
 	@Test(priority = 10, enabled = true)
@@ -153,30 +133,8 @@ public class MyTestCases extends Parameters {
 
 		Thread.sleep(15000);
 
-		WebElement Container = driver.findElement(By.xpath("//*[@id=\"__next\"]/div[2]/div[5]/div/div[2]"));
+		SortOptionChecker();
 
-		if (driver.getCurrentUrl().contains("en")) {
-			List<WebElement> PriceList = Container.findElements(By.cssSelector(
-					".MuiTypography-root.MuiTypography-heading3SemBld.__ds__comp.undefined.muiltr-18vmb2l"));
-			int LowestPrice = Integer.parseInt(PriceList.get(0).getText().replace("SAR ", ""));
-
-			int HighestPrice = Integer.parseInt(PriceList.get(PriceList.size() - 1).getText().replace("SAR ", ""));
-
-			boolean ActualPrice = LowestPrice < HighestPrice;
-			boolean ExpectedPrice = true;
-
-			Assert.assertEquals(ActualPrice, ExpectedPrice);
-		} else {
-			List<WebElement> PriceList = Container.findElements(By.cssSelector(
-					".MuiTypography-root.MuiTypography-heading3SemBld.__ds__comp.undefined.muirtl-1l5b3qq"));
-			int lowestPrice = Integer.parseInt(PriceList.get(0).getText().replace("ر.س. ", ""));
-			System.out.println();
-			int HighestPrice = Integer.parseInt(PriceList.get(PriceList.size() - 1).getText().replace("ر.س. ", ""));
-
-			boolean ActualValue = lowestPrice < HighestPrice;
-			boolean ExpectedValue = true;
-			Assert.assertEquals(ActualValue, ExpectedValue);
-		}
 	}
 
 }
--- src/Parameters.java
+++ src/Parameters.java
@@ -3,6 +3,7 @@ import java.io.IOException;
 import java.sql.Driver;
 import java.time.LocalDate;
 import java.util.Date;
+import java.util.List;
 import java.util.Random;
 
 import org.apache.commons.io.FileUtils;
@@ -12,6 +13,7 @@ import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 
 public class Parameters {
@@ -25,6 +27,7 @@ public class Parameters {
 	String ExpecttedNambur = "+966554400000";
 	boolean ExpecttedQitafLogo = true;
 	String ExpictedHotelTabIsNotSelect = "false";
+	boolean ExpectedThePageIsFullyLoaded = true;
 	int tomorrow = LocalDate.now().plusDays(1).getDayOfMonth();
 
 	String ExpictedDepature = String.format("%02d", tomorrow);
@@ -81,5 +84,60 @@ public class Parameters {
 
 		}
 		Thread.sleep(2000);
+	}
+
+	public void EnterNumberOfVisetorAndSearchResult() throws InterruptedException {
+		WebElement CitiesList = driver.findElement(By.cssSelector(".sc-phbroq-4.gGwzVo.AutoComplete__List"));
+		WebElement SelectNamberOfVistor = driver.findElement(By.cssSelector(".sc-tln3e3-1.gvrkTi"));
+		CitiesList.findElements(By.tagName("li")).get(1).click();
+
+		Select select = new Select(SelectNamberOfVistor);
+		int RandomOfVistor = rand.nextInt(2);
+
+		select.selectByIndex(RandomOfVistor);
+
+		WebElement SearchButton = driver.findElement(By.xpath("//button[@data-testid='HotelSearchBox__SearchButton']"));
+		SearchButton.click();
+
+		Thread.sleep(45000);
+	}
+
+	public void RandomlyCheckTheWebsite() {
+		driver.get(myWebsites[randomIndex]);
+
+		WebElement HotelTab = driver.findElement(By.id("uncontrolled-tab-example-tab-hotels"));
+
+		HotelTab.click();
+	}
+
+	public void SortOptionChecker() {
+
+		WebElement Container = driver.findElement(By.xpath("//*[@id=\"__next\"]/div[2]/div[5]/div/div[2]"));
+
+		if (driver.getCurrentUrl().contains("en")) {
+			List<WebElement> PriceList = Container.findElements(By.cssSelector(
+					".MuiTypography-root.MuiTypography-heading3SemBld.__ds__comp.undefined.muiltr-18vmb2l"));
+			int LowestPrice = Integer.parseInt(PriceList.get(0).getText().replace("SAR ", ""));
+
+			int HighestPrice = Integer.parseInt(PriceList.get(PriceList.size() - 1).getText().replace("SAR ", ""));
+
+			boolean ActualPrice = LowestPrice < HighestPrice;
+			boolean ExpectedPrice = true;
+
+			Assert.assertEquals(ActualPrice, ExpectedPrice);
+		
+		} else {
+			List<WebElement> PriceList = Container.findElements(By.cssSelector(
+					".MuiTypography-root.MuiTypography-heading3SemBld.__ds__comp.undefined.muirtl-1l5b3qq"));
+			int lowestPrice = Integer.parseInt(PriceList.get(0).getText().replace("ر.س. ", ""));
+			System.out.println();
+			int HighestPrice = Integer.parseInt(PriceList.get(PriceList.size() - 1).getText().replace("ر.س. ", ""));
+
+			boolean ActualValue = lowestPrice < HighestPrice;
+			boolean ExpectedValue = true;
+			Assert.assertEquals(ActualValue, ExpectedValue);
+
+		}
+
 	}
 }
